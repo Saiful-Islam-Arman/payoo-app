98c3c4d476c9790ccced6f815e1bec361f94ed2f
Saiful-Islam-Aman <saifulislamarmanr@gmail.com>
2024-09-19 22:39:37 +0600

    simple redirect

--- js/script.js
+++ js/script.js
@@ -7,8 +7,20 @@ document.getElementById('button-login')
         // step - 2 prevent default behavior ( prevetn reloading browser)
         event.preventDefault();
 
-        // step - 3 get the phone number
+        // step - 3 get the phone number & pin number
         const phoneNumber = document.getElementById('phone-number').value;
-        console.log(phoneNumber);
+        const pinNumber = document.getElementById('pin-number').value;
+        console.log(phoneNumber, pinNumber);
+
+        // validate phone number and pin
+        if(phoneNumber === '5' && pinNumber === '1234'){
+            window.location.href ='home.html';
+        }
+        else{
+            alert('wrong information');
+        }
+
+        // allow the user access
+
             
     })
